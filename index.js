4267009b37fe660a76f249be42b82c141f30bb00
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-05-23 01:43:47 +0600

    data shown in the display

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
+
 server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}/`);
+  console.log(`listening on http://localhost:${port}`);
 });
